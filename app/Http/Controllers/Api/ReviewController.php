49256c6c648483ac0cb59b490009ce348796963d
Chirag1Gaming <gaminggonewrong44@gmail.com>
2024-03-13 00:22:32 +0530

    models

--- app/Http/Controllers/Api/ReviewController.php
+++ app/Http/Controllers/Api/ReviewController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Model\Review;
+use App\Models\Review;
 
 class ReviewController extends Controller
 {
